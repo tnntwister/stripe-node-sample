282981055c3165d1c02af8bd086022832f2aea0a
François-Xavier Guillois <40766799+tnntwister@users.noreply.github.com>
2022-05-12 11:11:19 +0200

    first success

---
+++ views/payment.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv').config();
+const stripe = Stripe(process.env.STRIPE_TEST_PK); 
+const elements = stripe.elements();
+const card = elements.create("card", { hidePostalCode: true });
+card.mount("#payment-card");
+const form = document.querySelector("form");
+const errors = document.querySelector("#payment-errors");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  stripe.createToken(card).then((res) => {
+    if (res.error) {
+      errors.textContent = res.error.message;
+    } else {
+      const stripeToken = document.createElement("input");
+      stripeToken.setAttribute("type", "hidden");
+      stripeToken.setAttribute("name", "stripeToken");
+      stripeToken.setAttribute("value", res.token.id);
+      form.appendChild(stripeToken);
+      form.submit();
+    }
+  });
+});
