f5bd3b65281d1012f65a50551e9ed16e307bfe5a
François-Xavier Guillois <40766799+tnntwister@users.noreply.github.com>
2022-10-07 18:14:16 +0200

    clean assets test

--- assets/js/env.mjs
+++
@@ -1,5 +0,0 @@
-export default { 
-    'stripeSkKey': 'sk_test_51LqC9vJmZ3USYKbxorWjOecrETLcQ3ssFi1c8cgymLQsXvA8WsT72wLaHj6FR6brdNaHPY5X76rlEO8uxob6fibr001hBFp7b6', 
-    'stripePkKey':'pk_test_51LqC9vJmZ3USYKbx4ftxn0PLufcv4v7dAeSw9NBCN5cSfw2xCKaRQppVmyl558dUnR2iXAYtL8hGVsn6cd5RhprX00f0PPTzBz', 
-    'stripeCustomerId':'cus_MZLZcoQjE6JsvC'
-}
