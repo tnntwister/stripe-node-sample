d3d111eb2790526de6061701c3d96bb67555e592
François-Xavier Guillois <40766799+tnntwister@users.noreply.github.com>
2022-10-07 16:37:29 +0200

    dotenv and exploding pages

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -1,42 +1,25 @@
-const stripe = Stripe('pk_test_51LqC9vJmZ3USYKbx4ftxn0PLufcv4v7dAeSw9NBCN5cSfw2xCKaRQppVmyl558dUnR2iXAYtL8hGVsn6cd5RhprX00f0PPTzBz'); 
+require("dotenv").config();
+
+const stripe = Stripe(process.env.STRIPE_TEST_PK);
 const elements = stripe.elements();
 
-const card1 = card2 = elements.create("card", { hidePostalCode: true });
-card1.mount("#payment-card");
-const form1 = document.querySelector("#form1");
-const errors1 = document.querySelector("#payment-errors-1");
-card2.mount("#payment-card2");
-const form2 = document.querySelector("#form2");
-const errors2 = document.querySelector("#payment-errors-2");
+const card = elements.create("card", { hidePostalCode: true });
+card.mount("#payment-card");
+const form1 = document.querySelector("#form");
+const errors1 = document.querySelector("#payment-errors");
 
 form1.addEventListener("submit", (event) => {
-  event.preventDefault();
-  stripe.createToken(card1).then((res) => {
-    if (res.error) {
-      errors.textContent = res.error.message;
-    } else {
-      const stripeToken = document.createElement("input");
-      stripeToken.setAttribute("type", "hidden");
-      stripeToken.setAttribute("name", "stripeToken");
-      stripeToken.setAttribute("value", res.token.id);
-      form.appendChild(stripeToken);
-      form.submit();
-    }
-  });
-});
-
-form2.addEventListener("submit", (event) => {
-  event.preventDefault();
-  stripe.createToken(card2).then((res) => {
-    if (res.error) {
-      errors.textContent = res.error.message;
-    } else {
-      const stripeToken = document.createElement("input");
-      stripeToken.setAttribute("type", "hidden");
-      stripeToken.setAttribute("name", "stripeToken");
-      stripeToken.setAttribute("value", res.token.id);
-      form.appendChild(stripeToken);
-      form.submit();
-    }
-  });
+    event.preventDefault();
+    stripe.createToken(card).then((res) => {
+        if (res.error) {
+            errors.textContent = res.error.message;
+        } else {
+            const stripeToken = document.createElement("input");
+            stripeToken.setAttribute("type", "hidden");
+            stripeToken.setAttribute("name", "stripeToken");
+            stripeToken.setAttribute("value", res.token.id);
+            form.appendChild(stripeToken);
+            form.submit();
+        }
+    });
 });
--- index.js
+++ index.js
@@ -1,65 +1,126 @@
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const stripe = require('stripe')('sk_test_51LqC9vJmZ3USYKbxorWjOecrETLcQ3ssFi1c8cgymLQsXvA8WsT72wLaHj6FR6brdNaHPY5X76rlEO8uxob6fibr001hBFp7b6');
+const stripe = require("stripe")(process.env.STRIPE_TEST_SK);
 
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-// app.use(express.static('./views'));
-
-app.use(bodyParser.urlencoded({ extended: true }))
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/charge", (req, res) => {
-  try {
-    stripe.customers
-      .retrieve('cus_MZLTGUfhjfpBAS')
-      .then(customer => 
-        stripe.charges.create({
-          amount: req.body.amount * 100,
-          currency: "usd",
-          customer: customer.id
-        })
-      )
-      .then(() => res.render("success.html"))        
-      .catch(err => console.log(err));
-  } catch (err) {
-    res.send(err);
-  }
+    try {
+        stripe.customers
+            .retrieve(process.env.STRIPE_CUSTOMER_ID)
+            .then((customer) =>
+                stripe.charges.create({
+                    amount: req.body.amount * 100,
+                    currency: "usd",
+                    customer: customer.id,
+                })
+            )
+            .then(() => res.render("success.html"))
+            .catch((err) => console.log(err));
+    } catch (err) {
+        res.send(err);
+    }
 });
 
 app.post("/charge-new-customer", (req, res) => {
     try {
-      stripe.customers
-        .create({         
-          name: "compte test 1", 
-          email: req.body.email,
-          source: req.body.stripeToken
-        })
-        .then(customer =>
-          stripe.charges.create({
-            amount: req.body.amount * 100,
-            currency: "usd",
-            customer: customer.id
-          })
-        )
-        .then(() => res.render("success.html"))        
-        .catch(err => console.log(err));
+        stripe.customers
+            .create({
+                name: "compte test 1",
+                email: req.body.email,
+                source: req.body.stripeToken,
+            })
+            .then((customer) =>
+                stripe.charges.create({
+                    amount: req.body.amount * 100,
+                    currency: "usd",
+                    customer: customer.id,
+                })
+            )
+            .then(() => res.render("success.html"))
+            .catch((err) => console.log(err));
     } catch (err) {
-      res.send(err);
+        res.send(err);
     }
-  });
+});
 
-app.get('/', function(req, res) {
+app.get("/", function (req, res) {
     let customer = {};
     try {
-      stripe.customers
-        .retrieve('cus_MZLTGUfhjfpBAS')
-        .then((response) => res.render("index.html", { customer : response }))        
-        .catch(err => console.log(err));
+        stripe.customers
+            .retrieve("cus_MZLTGUfhjfpBAS")
+            .then((response) =>
+                res.render("index.html", { customer: response })
+            )
+            .catch((err) => console.log(err));
     } catch (err) {
-      res.send(err);
+        res.send(err);
     }
-  });
+});
 
-app.listen(3000, () => console.log('Server is running...'));
+app.get("/pay-with-existing-customer", function (req, res) {
+    let customer = {};
+    try {
+        stripe.customers
+            .retrieve("cus_MZLTGUfhjfpBAS")
+            .then((response) =>
+                res.render("index.html", { customer: response })
+            )
+            .catch((err) => console.log(err));
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.get("/pay-with-existing-customer", function (req, res) {
+    let customer = {};
+    try {
+        stripe.customers
+            .retrieve("cus_MZLTGUfhjfpBAS")
+            .then((response) =>
+                res.render("index.html", { customer: response })
+            )
+            .catch((err) => console.log(err));
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.get("/create-new-customer-and-pay", function (req, res) {
+    let customer = {};
+    try {
+        stripe.customers
+            .retrieve("cus_MZLTGUfhjfpBAS")
+            .then((response) =>
+                res.render("index.html", { customer: response })
+            )
+            .catch((err) => console.log(err));
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.get("/list-all-intents", async function (req, res) {
+    let customer = {};
+    try {
+        await stripe.charges
+            .list({
+                limit: 3,
+                customer: "cus_MZLZcoQjE6JsvC",
+            })
+            .then((response) =>
+                res.render("list-all-intents.html", { intents: response })
+            )
+            .catch((err) => console.log(err));
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.listen(3000, () => console.log("Server is running..."));
